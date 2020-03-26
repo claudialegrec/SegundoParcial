e6884dd3d2f522f683afc2ddd73c6824d572765b
Clau Alegre <claudialegrec.3@outlook.es>
2020-03-26 07:46:20 -0700

    se agregaron archivos

---
+++ manzanascript.js
@@ -0,0 +1,8 @@
+console.log("hola");
+
+function agergar5() {
+	console.log("sdjfb");
+	total = getElementById("txtActual").value;
+	total = total + 5;
+	document.getElementById("txtActual").value = total;
+}
